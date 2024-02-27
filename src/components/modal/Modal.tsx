a2b9e7ffb823d7a6caddf284e94971ff1b6fcddd
Felix-le <mc.vietanh@gmail.com>
2024-02-27 09:55:21 -0500

    done

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -5,8 +5,6 @@ import QuestionSection from "components/elements/QuestionsSection";
 import CheckBoxComponent from "../elements/CheckBoxComponent";
 import ButtonComponent from "../elements/ButtonComponent";
 
-const tools = ["Redux", "Lodash", "Ant design", "Webpack", "Other"];
-
 interface ModalProps {
   toolList: string[];
   toolsUsedArray: number[];
@@ -20,11 +18,18 @@ const Modal: React.FC<ModalProps> = ({
 }) => {
   const [isEditable, setIsEditable] = useState(false);
   const [isProficient, setIsProficient] = useState(isProficientDefault);
-  const [selectedTools, setSelectedTools] = useState<string[]>([]);
+  const [selectedTools, setSelectedTools] = useState<string[]>(
+    toolsUsedArray.map((index) => toolList[index])
+  );
 
   const handleSubmit = () => {
+    const formData = {
+      isProficient,
+      tools: toolList.join(", "),
+      toolsUsed: selectedTools.map((tool) => toolList.indexOf(tool)).join(","),
+    };
     // Handle submit logic
-    console.log("Submit clicked!");
+    console.log("Submit clicked!", formData);
   };
 
   return (
@@ -46,7 +51,7 @@ const Modal: React.FC<ModalProps> = ({
         subTitle="Please select all that apply."
       >
         <CheckBoxComponent
-          values={tools}
+          values={toolList}
           selectedValues={selectedTools}
           onChange={setSelectedTools}
           disabled={!isEditable}

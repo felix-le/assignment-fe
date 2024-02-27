988e1b98368d3623417168ab1b39a565caedb3a7
Felix-le <mc.vietanh@gmail.com>
2024-02-27 09:57:16 -0500

    done

--- src/components/modal/ModalWithContext.tsx
+++ src/components/modal/ModalWithContext.tsx
@@ -10,19 +10,6 @@ function ModalsWithContext() {
   return (
     <div className="flex justify-center items-center w-full flex-col pt-[10%]">
       <div className="h-full max-w-full bg-white p-[40px] min-w-[410px]">
-        {/* <div>ModalsWithContext</div>
-
-        <h1>Counter: {count}</h1>
-        <p>hello</p>
-
-        <Button
-          type="primary"
-          className="text-primary bg-brand"
-          onClick={() => dispatch(increment())}
-        >
-          Increment
-        </Button>
-        <Button onClick={() => dispatch(decrement())}>Decrement</Button> */}
         <Modal
           toolList={toolList}
           toolsUsedArray={toolsUsedArray}
